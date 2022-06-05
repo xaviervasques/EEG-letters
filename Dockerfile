13a992b00c7c3dcfeff75bc3a09572b77a6f7244
s94121angel <s94121angel@yahoo.com.tw>
2022-06-05 11:57:25 +0800

    remodeling 0605

--- Dockerfile
+++ Dockerfile
@@ -2,15 +2,20 @@ FROM jupyter/scipy-notebook
 
 RUN mkdir my-model
 ENV MODEL_DIR=/home/jovyan/my-model
+ENV LOCAL_PATH=/home/jovyan/data
 ENV MODEL_FILE_LDA=clf_lda.joblib
 ENV MODEL_FILE_NN=clf_nn.joblib
 
 RUN pip install joblib
 
-COPY train.csv ./train.csv
-COPY test.csv ./test.csv
+#COPY train.csv ./train.csv
+#COPY test.csv ./test.csv
 
 COPY train.py ./train.py
-COPY inference.py ./inference.py
+#COPY inference.py ./inference.py
 
-RUN python3 train.py
+
+
+#從本地資料夾內copy run.sh檔案
+COPY run.sh ./
+ENTRYPOINT ["/bin/bash", "./run.sh"]
--- train.py
+++ train.py
@@ -15,18 +15,37 @@ import pandas as pd
 from joblib import dump
 from sklearn import preprocessing
 
+
+
+initial_count=0  
+
+def count_number(): 
+    global initial_count
+    dir = "./data"
+    for path in os.listdir(dir):
+        if os.path.isfile(os.path.join(dir, path)):
+           initial_count += 1
+    return initial_count       
+
 def train():
-
+    
     # Load directory paths for persisting model
-
+    
     MODEL_DIR = os.environ["MODEL_DIR"]
     MODEL_FILE_LDA = os.environ["MODEL_FILE_LDA"]
     MODEL_FILE_NN = os.environ["MODEL_FILE_NN"]
-    MODEL_PATH_LDA = os.path.join(MODEL_DIR, MODEL_FILE_LDA)
-    MODEL_PATH_NN = os.path.join(MODEL_DIR, MODEL_FILE_NN)
+    
+    #Container內的模型儲存路徑
+    #MODEL_PATH_LDA = os.path.join(MODEL_DIR, MODEL_FILE_LDA)	    
+    #MODEL_PATH_NN = os.path.join(MODEL_DIR, MODEL_FILE_NN)
+    
+    #將模型儲存在mapping的資料夾
+    LOCAL_DIR = os.environ["LOCAL_PATH"]
+    #SAVE_AT_LOCAL_LDA = os.path.join(LOCAL_DIR, MODEL_FILE_LDA)
+    
       
     # Load, read and normalize training data
-    training = "./train.csv"
+    training = "./data/train.csv"
     data_train = pd.read_csv(training)
         
     y_train = data_train['# Letter'].values
@@ -42,20 +61,29 @@ def train():
     # Models training
     
     # Linear Discrimant Analysis (Default parameters)
-    clf_lda = LinearDiscriminantAnalysis()
-    clf_lda.fit(X_train, y_train)
+    #clf_lda = LinearDiscriminantAnalysis()
+    #clf_lda.fit(X_train, y_train)
     
     # Save model
     from joblib import dump
-    dump(clf_lda, MODEL_PATH_LDA)
+    #處存在Container內
+    #dump(clf_lda, MODEL_PATH_LDA)
+    #dump(clf_lda, SAVE_AT_LOCAL_LDA)
         
     # Neural Networks multi-layer perceptron (MLP) algorithm
     clf_NN = MLPClassifier(solver='adam', activation='relu', alpha=0.0001, hidden_layer_sizes=(500,), random_state=0, max_iter=1000)
     clf_NN.fit(X_train, y_train)
+    
        
     # Secord model
     from joblib import dump, load
-    dump(clf_NN, MODEL_PATH_NN)
+    #dump(clf_NN, MODEL_PATH_NN)
+    
+    #得到回傳值(資料夾內檔案數量)
+    count_of_file=count_number()
+    NAME_with_count=str(count_of_file)+"_"+MODEL_FILE_NN
+    SAVE_AT_LOCAL_NN =os.path.join(LOCAL_DIR,  NAME_with_count)
+    dump(clf_NN, SAVE_AT_LOCAL_NN)
         
 if __name__ == '__main__':
     train()
